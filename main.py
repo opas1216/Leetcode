74857e8cc229411a8e062ffaaf704e6f09997464
Michael <opas1216@hotmail.com>
2022-12-12 17:31:13 +0800

    Update Daily Practice

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# Leetcode #724
 def pivotIndex(nums: list[int]) -> int:
     print(f"nums = {nums}")
     summation = 0
@@ -20,18 +21,99 @@ def pivotIndex(nums: list[int]) -> int:
                 return index
     return -1
 
-
+# Leetcode #205
 def isIsomorphic(s: str, t: str) -> bool:
-    print(f"Origin -- s={s}, t={t}")
-    for index, alpha in enumerate(s):
-        print(f"index={index}, alpha={alpha}, t[index] = {t[index]}")
-        t = t.replace(t[index], alpha)
-    print(f"After replace -- s={s}, t={t}")
+    print(f"s={s}, t={t}")
+    # collect_dict_a, collect_dict_b = {}, {}
+    # index_to_alpha_dict = {}
+    # for index, alpha in enumerate(s):
+    #     if alpha not in collect_dict_a:
+    #         collect_dict_a[alpha] = [index]
+    #     else:
+    #         collect_dict_a[alpha].append(index)
+    #
+    # for index, alpha in enumerate(t):
+    #     if alpha not in collect_dict_b:
+    #         collect_dict_b[alpha] = [index]
+    #     else:
+    #         collect_dict_b[alpha].append(index)
+    #     index_to_alpha_dict[index] = alpha
+    #
+    # if len(collect_dict_a) == len(collect_dict_b):
+    #     for key, value in collect_dict_a.items():
+    #         print(f"Key = {key}, value = {value}, len of value = {len(value)}")
+    #         if len(value)>1:
+    #             for index, element in enumerate(value):
+    #                 print(f"Index and Element of Enumerate(value) = {index} & {element}")
+    #                 if index == 0:
+    #                     compare_alpha = index_to_alpha_dict[element]
+    #                 else:
+    #                     print(f"compare_alpha = {compare_alpha}, index_to_alpha_dict[index] = {index_to_alpha_dict[element]}")
+    #                     if compare_alpha != index_to_alpha_dict[element]:
+    #                         return False
+    #     return True
+    # else:
+    #     print(f"The amount of distinct characters from s & t is different.")
+    #     return False
 
-    if t == s :
-        return True
-    else:
-        return False
+    character_list_s, character_list_t = [], []
+    character_dict = {}
+    # Can use Set to replace the below purpose.
+    # for character in s:
+    #     if character not in character_list_s:
+    #         character_list_s.append(character)
+    # for character in t:
+    #     if character not in character_list_t:
+    #         character_list_t.append(character)
+    # if len(s) != len(t) or len(character_list_s) != len(character_list_t): return False
+
+    if len(s) != len(t) or len(set(s)) != len(set(t)): return False
+
+    for index, character in enumerate(s):
+        if character not in character_dict:
+            character_dict[character] = t[index]
+        else:
+            if character_dict[character] != t[index]: return False
+    return True
+
+# Leetcode #392
+def isSubsequence(s: str, t: str) -> bool:
+    ############# My Solution
+    # current_index = 0
+    # for index, character in enumerate(s):
+    #     character_match = False
+    #     for target_index in range(current_index, len(t)):
+    #         if character == t[target_index]:
+    #             print(f"character from s {character} is equal to target character {t[target_index]} from t at index {target_index}")
+    #             current_index = target_index + 1
+    #             character_match = True
+    #             break
+    #         if target_index == len(t) and index < len(s)-1:
+    #             print(f"Target index {target_index} reach the length of t {len(t)}, but the major index is at {index}")
+    #             return False
+    #     if character_match == False: return False
+    # return True
+
+    #Better way from leetcode
+    index = 0
+    for character in s:
+        if character in t[index:]:
+            t = t[t[index:].index(character)+1:]
+        else:
+            return False
+    return True
+
+# Leetcode #21
+# Merge Two Sorted Lists
+#
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+
 
 
 
@@ -43,14 +125,13 @@ if __name__ == '__main__':
     # print("Result = {}".format(pivotIndex([1,2,3])))
     # print("Result = {}".format(pivotIndex([-1,-1,-1,-1,-1,0])))
     # print("Result = {}".format(pivotIndex([1, 7, 3, 6, 5, 6])))
-    print(f"Result = {isIsomorphic('egg', 'add')}")
-    print(f"Result = {isIsomorphic('foo', 'bar')}")
-    print(f"Result = {isIsomorphic('paper', 'title')}")
-
-
-# Press the green button in the gutter to run the script.
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
+    # print(f"Result = {isIsomorphic('egg', 'add')}")
+    # print(f"Result = {isIsomorphic('foo', 'bar')}")
+    # print(f"Result = {isIsomorphic('paper', 'title')}")
+    # print(isSubsequence("abc", "ahbgdc"))
+    # print(isSubsequence("axc", "ahbgdc"))
+    solution = Solution
+    print(mergeTwoLists)
 
 
 
